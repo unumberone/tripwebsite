f88b1ee3b46a0ddc2f66a14227f3b62d09215343
unumberone <sonnguyen31032003@gmail.com>
2024-07-31 18:11:19 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css"
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Popular from "./Components/Popular/Popular";
+import Offers from "./Components/Offers/Offers";
+import About from "./Components/About/About";
+import Blog from "./Components/Blog/Blog";
+import Footer from "./Components/Footer/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return(
+        <>
+        <Navbar></Navbar>
+        <Home></Home>
+        <Popular></Popular>
+        <Offers></Offers>
+        <About></About>
+        <Blog></Blog>
+        <Footer></Footer>
+        </>
+    )
 }
 
-export default App;
+export default App

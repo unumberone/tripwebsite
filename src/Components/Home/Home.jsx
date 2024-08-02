b49fe97428be5d609d8f86b3da9a1de4a50b42cc
unumberone <sonnguyen31032003@gmail.com>
2024-08-02 11:46:28 +0700

    update nhóm các html, thêm chức năng cho toggle, thêm tính năng tìm kiếm

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import './home.css'
+
+
  
 const Home = () => {
     return (
@@ -9,12 +11,33 @@ const Home = () => {
                     <h1 className="title">
                         Plan Your Trip With Travel Dot
                     </h1>
-                    <p className="subTitlte">
+                    <p className="subTitle">
                         Travel to your favourite city with respectful of the environment!
                     </p>
-                    <button className="btn">
-                        <a href="#">Explore Now</a>
+                    <button className="mainbtn">
+                        <a href="#" className="mainText">Explore Now</a>
                     </button>
+                </div>
+
+                <div className="homeCard grid">
+
+                    <div className="locationDiv">
+                        <label htmlFor="location">Location</label>
+                        <input type="text" placeholder="Nhập địa điểm muốn đến...."/>
+                    </div>
+
+                    <div className="distDiv">
+                        <label htmlFor="distance">Location</label>
+                        <input type="text" placeholder="Hà Nội,..."/>
+                    </div>
+
+                    <div className="priceDiv">
+                        <label htmlFor="price">Location</label>
+                        <input type="text" placeholder="800.000VND-100.000.000VND"/>
+                    </div>
+                    <button className="mainbtn"></button>
+
+
                 </div>
             </div>
         </section>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,66 +1,55 @@
 import React, { useState } from "react";
 import "./navbar.css";
 import { MdTravelExplore } from "react-icons/md";
-import { AiFillCloseCircle } from "react-icons/ai";
 import { TbGridDots } from "react-icons/tb";
 
 const Navbar = () => {
   // State to manage the visibility of the navbar
-  const [active, setActive] = useState('navBar');
+  const [isNavVisible, setIsNavVisible] = useState(false);
 
-  // Show navbar
-  const showNav = () => {
-    setActive('navBar activeNavbar');
-  };
-
-  // Remove navbar
-  const removeNav = () => {
-    setActive('navBar');
+  // Toggle navbar visibility
+  const toggleNav = () => {
+    setIsNavVisible(prevState => !prevState);
   };
 
   return (
     <section className="navBarSection">
       <div className="header">
         <div className="logoDiv">
-          <div className={active}>
-            <ul className="navLists">
-              <li className="navItem">
-                <a href="#" className="navLink">Home</a>
-              </li>
-              <li className="navItem">
-                <a href="#" className="navLink">Products</a>
-              </li>
-              <li className="navItem">
-                <a href="#" className="navLink">Resources</a>
-              </li>
-              <li className="navItem">
-                <a href="#" className="navLink">Contact</a>
-              </li>
-              <li className="navItem">
-                <a href="#" className="navLink">Blog</a>
-              </li>
-              <div className="headerBtns">
-                <button className="btn loginBtn">
-                  <a href="#" className="">Login</a>
-                </button>
-                <button className="btn loginBtn">
-                  <a href="#" className="">Sign Up</a>
-                </button>
-                <div onClick={removeNav} className="closeNavbar">
-                  <AiFillCloseCircle className="icon" />
-                </div>
-                <div onClick={showNav} className="toggleNavbar">
-                  <TbGridDots className="icon" />
-                </div>
-              </div>
-            </ul>
-          </div>
           <a href="#" className="logo">
-            <h1 className="flex">
-              TravelMate <MdTravelExplore className="icon" />
-            </h1>
+            <h1 className="flex">TravelMate<MdTravelExplore className="icon" /></h1>
           </a>
+          <div className="headerBtns">
+            <button className="btn loginBtn">
+              <a href="#" className="">Login</a>
+            </button>
+            <button className="btn loginBtn">
+              <a href="#" className="">Sign Up</a>
+            </button>
+            <div onClick={toggleNav} className="toggleNavbar">
+              <TbGridDots className="icon" />
+            </div>
+          </div>
         </div>
+      </div>
+      <div className={isNavVisible ? 'navBar activeNavbar' : 'navBar'}>
+        <ul className="navLists">
+          <li className="navItem">
+            <a href="#" className="navLink">Home</a>
+          </li>
+          <li className="navItem">
+            <a href="#" className="navLink">Products</a>
+          </li>
+          <li className="navItem">
+            <a href="#" className="navLink">Resources</a>
+          </li>
+          <li className="navItem">
+            <a href="#" className="navLink">Contact</a>
+          </li>
+          <li className="navItem">
+            <a href="#" className="navLink">Blog</a>
+          </li>
+        </ul>
       </div>
     </section>
   );
